7767011d837fd074cff9a0600bdade1c5026457a
Moriah Young <58644914+myoung836@users.noreply.github.com>
2019-12-11 13:51:36 -0500

    Update Virus.cpp

--- Virus.cpp
+++ Virus.cpp
@@ -22,4 +22,16 @@ int Virus::getY()
   return y;
 }
 
+void Virus::setX(int X_){
+  if(X_ > 0 && X_ < 10)
+  {
+    x = X_;
+}
+  
+ void Virus::setY(int Y_){
+  if(Y_ > 0 && Y_ < 10)
+  {
+    Y = Y_;
+}
+
 
