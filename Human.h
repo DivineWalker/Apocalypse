858f79df23016f1fd1bc09009b2cf44e51628abd
DivineWalker <49419110+DivineWalker@users.noreply.github.com>
2019-12-11 11:46:02 -0500

    Add files via upload

--- Human.h
+++ Human.h
@@ -1,1 +1,37 @@
+//#include "World.h"
+#include <iostream>
 
+using namespace std;
+
+class Human{
+public:
+	Human();
+	Human(int);
+	//void Position();
+	int getX();
+	int getY();
+	void setX(int);
+	void setY(int);
+	void Attributes();
+	void setGender(string);
+	string getGender();
+	double getImmunity();
+	void operator+(Human);
+	//void PrintGrid();
+
+
+protected:
+	void initialPosition();
+	bool CanHaveChildren();
+	void randomizeGender();
+	string gender;
+	int x,y;
+
+private:
+	int age;
+	bool married;
+	//String gender;
+	
+	double health;
+	double immunity;
+};
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include "sim.h"
+#include "World.h"
+//#include "Human.h"
+
+using namespace std;
+
+int main(){
+	sim();
+	
+	return 0;
+}
