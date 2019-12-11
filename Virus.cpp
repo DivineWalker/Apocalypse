08ad7aec90e11610394ab9b3a7ebae4f31465d4f
Moriah Young <58644914+myoung836@users.noreply.github.com>
2019-12-11 13:36:11 -0500

    Update Virus.cpp

--- Virus.cpp
+++ Virus.cpp
@@ -1,1 +1,25 @@
+#include "Virus.h"
+
+Virus::Virus()
+{
+  //int x = 0;
+  //int y = 0;
+}
+
+Virus::Position()
+{
+  x = rand()% 10;
+  y = rand()% 10;
+}
+
+int Virus::getX()
+{
+  return x;
+}
+
+int Virus::getY()
+{
+  return y;
+}
+
 
