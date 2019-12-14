cdb76cc789813b3246986fc8d541d1108e15ce0d
Moriah Young <58644914+myoung836@users.noreply.github.com>
2019-12-14 03:17:50 -0500

    Update Description.h

--- Description.h
+++ Description.h
@@ -8,8 +8,10 @@ is illegal and no move will be made. As the humans move, if two humans come into
 genders, on top of being unmarried and over the age of 18, a new human with a randomly assigned gender is spawned on a random 
 spot on the grid. If the two humans do not meet the criteria for spawning children, then the human will move until it finds its
 own space. Be mindful that the human objects have an immunity value and if a human and a virus object come into contact then their 
-immunity, if greater than 0.25,  will be subtracted by a randomized number between 0 and 1. If their immunity is less than 0.25, 
-then the human will gain a symbol, !,  that differentiates it from the uninfected humans. Spawned humans do acquire the combined 
+immunity, if greater than 0.25, will be subtracted by a randomized number between 0 and 1. If their immunity is less than 0.25, 
+then the human will gain a symbol, !, that differentiates it from the uninfected humans. Spawned humans do acquire the combined 
 immunity of their parents. If a human comes into contact with an infected human, they will be attacked as if they came into contact 
 with the virus and will act accordingly as if they have a virus. The humans will age as the simulation continues and once the age of 
-65 is reached, their health will decrease by 0.5 each year. 
+65 is reached, their health will decrease by 0.5 each year leading to death. The grid will print out each time a move is made, a human
+is spawned, another virus appeards, a human is attacked or infected and dying. The user will be informed of all changed either shown 
+on the board itself or displayed below the board
